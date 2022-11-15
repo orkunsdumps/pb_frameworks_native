30b15a49a016dfe186a19db625ba1e6c9331b370
DarkJoker360 <simoespo159@gmail.com>
2022-11-15 11:44:22 +0100

    REThreaded: Use gen- and delete- textures on all render engines
    * Fixes bootanimation lags.
    
    This reverts commit https://github.com/begonia-dev/android_frameworks_native/commit/48b5f3cbeae031324406ccbcb037651c66611adb.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- libs/renderengine/threaded/RenderEngineThreaded.cpp
+++ libs/renderengine/threaded/RenderEngineThreaded.cpp
@@ -179,10 +179,6 @@ void RenderEngineThreaded::dump(std::string& result) {
 
 void RenderEngineThreaded::genTextures(size_t count, uint32_t* names) {
     ATRACE_CALL();
-    // This is a no-op in SkiaRenderEngine.
-    if (getRenderEngineType() != RenderEngineType::THREADED) {
-        return;
-    }
     std::promise<void> resultPromise;
     std::future<void> resultFuture = resultPromise.get_future();
     {
@@ -199,10 +195,6 @@ void RenderEngineThreaded::genTextures(size_t count, uint32_t* names) {
 
 void RenderEngineThreaded::deleteTextures(size_t count, uint32_t const* names) {
     ATRACE_CALL();
-    // This is a no-op in SkiaRenderEngine.
-    if (getRenderEngineType() != RenderEngineType::THREADED) {
-        return;
-    }
     std::promise<void> resultPromise;
     std::future<void> resultFuture = resultPromise.get_future();
     {
